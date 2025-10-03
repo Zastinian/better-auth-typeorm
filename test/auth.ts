42f07464ee1dcb0358f40763a4a61ab7921ce2b2
zastinian <zastinian@gmail.com>
2025-10-02 20:46:02 -0600

    test: Update testing

--- test/auth.ts
+++ test/auth.ts
@@ -3,11 +3,11 @@ import { betterAuth } from "better-auth";
 import { typeormAdapter } from "@hedystia/better-auth-typeorm";
 import path from "path";
 import { organization } from "better-auth/plugins";
-import { twoFactor } from "better-auth/plugins"
+import { twoFactor } from "better-auth/plugins";
 
 const dataSource = new DataSource({
   type: "sqlite",
-  database: path.resolve("./db.sqlite"),
+  database: path.join(__dirname, "./db.sqlite"),
   migrationsRun: true,
   entities: [path.join(__dirname, "typeorm/entities/**/*.ts")],
   migrations: [path.join(__dirname, "typeorm/migrations/**/*.ts")],
